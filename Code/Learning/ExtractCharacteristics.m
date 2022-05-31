e071a2ef2f814cc860c9c95503f096fd20cdccf4
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2022-05-31 13:18:31 +0200

    more characteristics

--- Code/Learning/ExtractCharacteristics.m
+++ Code/Learning/ExtractCharacteristics.m
@@ -1,10 +1,24 @@
 function [characteristics] = ExtractCharacteristics(grayImage)
-    [r, c, channels] = size(grayImage);
+    [~, ~, channels] = size(grayImage);
     if(channels == 3) % RGB image
         grayImage = rgb2gray(grayImage);
     end
     I = imresize(grayImage, [48, 32]);
+    % mean gray level
     meanGray = mean2(I);
-    characteristics = [meanGray];
+    % mean gradient
+    h = fspecial("sobel");
+    Sy = imfilter(I, h);
+    Sx = imfilter(I, h');
+    Ider = abs(Sy) + abs(Sx);
+    meanGradient = mean2(Ider);
+    % number of brightest
+    % eyes should have a lot of white pixels
+%     hist = imhist(I);
+%     brightest = find(hist, 1, 'last');
+%     nBrightest = hist(brightest);
+    brightest = I > 200;
+    nBrightest = sum(sum(brightest));
+    characteristics = table(meanGray, meanGradient, nBrightest);
 end
 
