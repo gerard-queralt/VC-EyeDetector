5d56cf427f1278cefc526e661c783662d3e56e25
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2022-06-08 12:58:18 +0200

    working on descriptors
    removed BI descriptors

--- Code/Learning/ExtractCharacteristics.m
+++ Code/Learning/ExtractCharacteristics.m
@@ -4,29 +4,22 @@ function [characteristics] = ExtractCharacteristics(grayImage)
         grayImage = rgb2gray(grayImage);
     end
     I = imresize(grayImage, [48, 32]);
-    % mean gray level
-    meanGray = mean2(I);
     % mean gradient
     h = fspecial("sobel");
     Sy = imfilter(I, h);
     Sx = imfilter(I, h');
     Ider = abs(Sy) + abs(Sx);
     meanGradient = mean2(Ider);
-    
-    % working with largest connected component
-    BI = not(imbinarize(I));
-    filteredBiggest = bwareafilt(BI,1);
+    % graylevel histogram
+    hist = imhist(I)';
+    % sum of columns
+    sumCol = sum(I,1);
+    % sum of rows
+    sumRow = sum(I,2)';
+    % gray-level co-occurrence matrix
+    glcms = graycomatrix(I);
+    glcmsVec = reshape(glcms, 1, []);
 
-    prop = regionprops('table', filteredBiggest, I, 'WeightedCentroid', ...
-                       'MajorAxisLength', 'MinorAxisLength','MaxFeretProperties', 'MinFeretProperties');
-    centroidX = prop.WeightedCentroid(1);
-    centroidY = prop.WeightedCentroid(2);
-    meanAxisLength = mean([prop.MajorAxisLength prop.MinorAxisLength], 2);
-    meanFeret = mean([prop.MaxFeretDiameter prop.MinFeretDiameter], 2);
-    meanAxisFeretRatio = meanAxisLength / meanFeret;
-    BIchars = table(centroidX, centroidY, meanAxisFeretRatio, meanAxisLength, meanFeret);
-
-    characteristics = table(meanGray, meanGradient);
-    characteristics = [characteristics, BIchars];
+    characteristics = table(meanGradient, hist, sumCol, sumRow, glcmsVec);
 end
 
